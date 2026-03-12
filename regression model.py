b304f8657363b4d74530f990e2aae9487e052a11
VEDANT   MAHAJAN <92252425+vedmah@users.noreply.github.com>
2026-03-12 22:36:07 +0530

    Update regression model.py

--- regression model.py
+++ regression model.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import plotly.express as px
